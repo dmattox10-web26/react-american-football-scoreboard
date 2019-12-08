de4ce0872b32692f74361ba7f19c170b260aef1f
Daniel Mattox <dmattox10@gmail.com>
2019-12-08 16:12:43 -0500

    Finished BottomRow with props

--- src/BottomRow.js
+++ src/BottomRow.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 
 const BottomRow = () => {
+  const { quarter } = this.props
   return (
     <div className="bottomRow">
       <div className="down">
@@ -18,7 +19,7 @@ const BottomRow = () => {
       </div>
       <div className="quarter">
         <h3 className="quarter__title">Quarter</h3>
-        <div className="quarter__value">4</div>
+        <div className="quarter__value">{ quarter }</div>
       </div>
     </div>
   );
